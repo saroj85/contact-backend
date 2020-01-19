2c0a54ad8861dd178804201322035935298a45ee
saroj kumar <send4saroj@gmail.com>
2020-01-19 20:28:38 +0530

    Update index.js

--- transporter/index.js
+++ transporter/index.js
@@ -3,7 +3,7 @@ let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: 'web4satyendra@gmail.com', // TODO: your gmail account 
-        pass: 'satyendra@852131' // TODO: your gmail password
+        pass: 'satyendra' // TODO: your gmail password
     }
 });
 
