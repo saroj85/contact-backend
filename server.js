bfbb4476ab242b447ee363fa89369c3ada6cb7e0
saroj kumar <send4saroj@gmail.com>
2020-02-25 12:05:07 +0530

    server.js
    add cross origin

--- server.js
+++ server.js
@@ -17,6 +17,26 @@ connectDB();
 // init middleweare  for post request 
 app.use(express.json({ extended: false }));
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
 app.use('/api/users', users);
 app.use('/api/auth', auth);
 app.use('/api/contacts', contacts);
